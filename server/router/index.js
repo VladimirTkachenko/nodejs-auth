4030cbad3a442b99ecda3f6ed527dbeae217a4bf
VladimirTkachenko <tkachenko.vladimir.kh@gmail.com>
2022-05-20 16:39:00 +0300

    commit-1

--- server/router/index.js
+++ server/router/index.js
@@ -7,7 +7,7 @@ router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/login/activate/:link', userController.activate);
 router.post('/logout', userController.logout);
-router.get('/refresh');
-router.get('/users');
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getUsers);
 
 module.exports = router;
